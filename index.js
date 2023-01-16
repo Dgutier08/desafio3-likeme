960382fa9b66f0af7643c10b935a7238d17ac75c
Dgutier08 <102337196+Dgutier08@users.noreply.github.com>
2023-01-15 22:22:34 -0300

    add-servicio delete y put

--- index.js
+++ index.js
@@ -1,39 +1,62 @@
-const express = require ('express')
-const app = express ();
-const { obtenerPosts, agregarLike } = require ('./posts')
+const express =  require('express');
+const app = express();
 
-const cors = require('cors');
-app.use(cors());
 
-app.use(express.static("public")); //midelware consulta
-app.use(express.json()); //midelware public
+const { obtenerPots, crearPost, agregarLike, deletePost, getPost } = require('./posts');
 
-app.get("/posts", (req, res)  => {
-try {
+app.use(express.json()) //parsear consultas
+app.use(express.static("public")); //Archivos estaticos
+
+
+
+app.get("/", (req, res) => {
+  try {
     res.sendFile();
-    } catch (error){
-        res.json({mesasge:"No se encuentra el recurso"})
-    }
-})
+  } catch (error) {
+    res.json({ message: "No se encuentra el recurso que estas solicitando" });
+  }
+});
 
 app.get("/posts", async (req, res) => {
-    try {
-      const getPosts = await obtenerPosts();
-      res.json(getPosts);
-    } catch (error) {
-      console.log(error);
-    }
-  });
+  try {
+    const getPosts = await obtenerPots();
+    res.json(getPosts);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
- app.post ("/posts",async (req, res)=> {
-    try {
-        const {titulo, url, descricion} = req.body
-        await agregarLike(titulo, url, descricion)
-        res.send("Post creado")
-    }catch (error){
-        console.log(error)
-    }
- } ) 
+app.post("/posts", async (req, res) => {
+  try {
+    const { titulo, url, descripcion } = req.body;
+    await crearPost(titulo, url, descripcion);
+    res.send("Post creado");
+  } catch (error) {
+    console.log(error);
+  }
+});
 
- app.listen (3000,console.log ("El servidor esta escuchando en el puerto 3000 "))
+app.put('/posts/like/:id',  async (req, res) => {
+  const { id } = req.params;
+  try {
+    await agregarLike(id);
+    res.send('like added');
+  } catch (error) {
+    res.send(error);
+    res.status(500).json({ message: "Servicio no disponible para hacer like " })
+  }
+});
 
+app.delete('/posts/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    await deletePost(id);
+    res.send('Post eliminado en exito');
+  } catch (error) {
+    res.status(500).json({ message: "no se puede eliminar reistro " })
+    
+  }
+});
+
+
+app.listen(3000, console.log("El servidor esta escuchando en el puerto 3000 "))
--- posts.js
+++ posts.js
@@ -1,28 +1,44 @@
-const { Pool } = require ('pg')//conectamos a la base de datos
+const { Pool } = require('pg');
 
-const pool = new Pool ({
-    host:'localhost',
-    user:'postgres',
-    password:'da',
-    database:'likeme',
-    allowExitOnIdle: true //cierre automatico de conexiones
-})
+const pool = new Pool({
+  host: 'localhost',
+  user: 'postgres',
+  password: 'da',
+  database: 'likeme',
+  allowExitOnIdle: true  
+});
 
-const obtenerPosts = async () => {
-    const result = await pool.query("SELECT * from posts")
-    console.log(result)
+const obtenerPots = async() => {
+  const { rows } = await pool.query("SELECT * FROM posts");
+  return rows;
+};
+
+const getPost = async(id) => {
+  const consulta = "SELECT FROM posts WHERE id = $1";
+  const values = [id];
+  const result = await pool.query(consulta, values);
+  const [registro] = result.rows;
 }
 
-//getDate ()
+const crearPost = async (titulo, img, descripcion) => {
+  const consulta = "INSERT INTO posts values (DEFAULT, $1, $2, $3)";
+  const values = [titulo, img, descripcion];
+  const result = await pool.query(consulta, values);
+};
 
+const agregarLike = async (id) => {
+  const consulta = 'UPDATE posts SET likes = (case when likes IS NULL then 1 else likes + 1 end) WHERE id = $1;'
+  const values = [id];
+  const { rowCount } = await pool.query(consulta, values)
+    if (rowCount === 0) {
+        throw { code: 404, message: `No se consiguió ningún viaje con el id ${id}` }
+    }
+};
 
-//registramos los likes
+const deletePost = async(id) => {
+  const consulta = 'DELETE FROM posts WHERE id = $1'
+  const values = [id];
+  const result = await pool.query(consulta, values);
+};
 
-const agregarLike = async (titulo, img, descripcion) => {
-    const consulta = "INSERT INTO posts values  (DEFAULT, $1, $2, $3)"
-    const values = [titulo, img, descripcion]
-    const result = await pool.query(consulta,values)
-    //console.log ("Like agregado")
-}
-
-module.exports = { obtenerPosts, agregarLike };
+module.exports = { obtenerPots, crearPost, agregarLike, deletePost, getPost };
